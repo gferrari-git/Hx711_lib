e58110b1591431568888601d74940ad891acb5c1
gferrari-git <63431381+gferrari-git@users.noreply.github.com>
2021-05-14 00:14:44 -0300

    works in simulator

--- main.c
+++ main.c
@@ -5,12 +5,23 @@
 
 #include <xc.h>
 #include <stdint.h>
+#include <stdio.h>
 #include "user.h"
 #include "system.h"
+#include "hx711.h"
 
 
 void main(void){
-    
+    int32_t value;
+    initApp();
+    uartConfig();
+    hx711Init();
+    while(1){
+        value=hx711Read();
+        printf("%ld\r\n",value);  
+        __delay_ms(500);
+                
+    }
     
 }
 
